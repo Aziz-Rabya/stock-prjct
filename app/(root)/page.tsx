eca5197482e477761b53dab97a5d4d53acd0d307
Aziz-Rabya <azizrabya81@gmail.com>
2025-11-21 12:40:04 +0100

    implement header

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,11 @@
+import Header from "@/components/Header"
 import { Button } from "@/components/ui/button"
 
 
 const Home = () => {
   return (
-    <div className="flex justify-center items-center h-screen">
-      <Button>click me</Button>
+    <div className="flex min-h-screen home-wrapper">
+      <Header />
     </div>
   )
 }
